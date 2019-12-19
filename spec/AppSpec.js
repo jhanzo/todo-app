d5c87397bd77f3b897e0562c46be142e6085ba18
jhanzo <jessy.hanzo@gmail.com>
2019-12-19 15:42:04 +0100

    replace project

---
+++ spec/AppSpec.js
@@ -0,0 +1,13 @@
+describe('app', function () {
+	'use strict';
+
+	describe('todo describe', function () {
+		it('todo it 1', function () {
+			expect(true).toBe(true);
+		});
+
+               it('todo it 2', function () {
+                        expect(false).toBe(true);
+               });
+	});
+});
